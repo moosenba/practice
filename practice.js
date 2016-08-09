461a3c80a5c4d2dac5690dd9682fc2351171db1c
Moose Enba <Moose Enba>
2016-08-08 21:16:59 -0400

    Added new functionality to tests

--- practice.js
+++ practice.js
@@ -5,8 +5,8 @@ var client = webdriverio.remote(options);
 client
     .init()
     .url('https://www.google.com/')
-    // .setValue('#search_form_input_homepage', 'WebdriverIO')
-    // .click('#search_button_homepage')
+    .setValue('#lst-ib', 'WebdriverIO')
+    .click('[name="btnG"]')
     .getTitle().then(function(title) {
         console.log('Title is: ' + title);
     })
